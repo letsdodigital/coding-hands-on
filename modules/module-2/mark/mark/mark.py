df7441739a8fd783adacf0faf342eb27630e82f2
cotswoldsmaker <mark.allan.bailey@gmail.com>
2024-04-29 10:40:38 +0100

    Add form functionality to consent form page

--- modules/module-2/mark/mark/mark.py
+++ modules/module-2/mark/mark/mark.py
@@ -3,6 +3,14 @@ from rxconfig import config
 import reflex as rx
 
 
+class FormState(rx.State):
+    form_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        """Handles the form submit."""
+        self.form_data = form_data
+
+
 @rx.page(title="Consent form")
 def index():
     return rx.center(
@@ -12,6 +20,23 @@ def index():
                 padding_top="20px",
             ),
             rx.text("This is where I build the consent form"),
+            rx.form(
+                rx.vstack(
+                    rx.input(
+                        placeholder="Test text...",
+                        name="test_data",
+                        max_length="100",
+                        required=True,
+                    ),
+                    rx.button("Submit", type="submit"),
+                    spacing="4",
+                ),
+                on_submit=FormState.handle_submit,
+                reset_on_submit=True,
+            ),
+            rx.divider(),
+            rx.heading("Results"),
+            rx.text(FormState.form_data.to_string()),
             spacing="4",
         ),
         width="100%",
