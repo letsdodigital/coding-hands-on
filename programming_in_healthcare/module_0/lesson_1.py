6a1d7bd0a4e02aa11442c92c38d8a5af5225eace
cotswoldsmaker <mark.allan.bailey@gmail.com>
2024-07-03 08:27:56 +0100

    module 0 added

---
+++ programming_in_healthcare/module_0/lesson_1.py
@@ -0,0 +1,39 @@
+""" Lesson 1 
+
+Some basic terminal and python exercises
+
+Gotten stuck, look at the slides at:
+https://letsdodigital.org/learn/learn-python/module-1/2-coding-environment.html
+"""
+
+"""Exercise 1
+Just run this python script.
+"""
+
+
+"""Exercise 2
+Change the output from the print function to print your name.
+"""
+
+print("Hello world!")
+
+
+"""Exercise 3
+Run a terminal command to show all files and folders in your current directory.
+"""
+
+
+"""Exercise 4
+1. Run a terminal command to take you back one level in the directory path.
+2. Run a terminal command to take you back to where you started.
+"""
+
+
+"""Exercise 4
+Run a terminal command to show what is your current directory path.
+"""
+
+"""Finished
+Let your tutor know when you have finished the above. We will start the next 
+session when everyone is finished with this exercise.
+"""
