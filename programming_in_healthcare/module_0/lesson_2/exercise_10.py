eb8cd09e0e245a7873418ef8a95de573a34ad2ff
cotswoldsmaker <mark.allan.bailey@gmail.com>
2024-07-08 13:53:22 +0100

    feat: Update clinical calculator equation in exercise 10

--- programming_in_healthcare/module_0/lesson_2/exercise_10.py
+++ programming_in_healthcare/module_0/lesson_2/exercise_10.py
@@ -1,12 +1,11 @@
 """Lesson 2 - exercise 9 - your call
 
     Build your own clinical calculator. Use your favourite search engine to find 
-    a clinical calculator of your choice.
+    a clinical calculator equation of your choice.
 
     This time, RETURN the result of the calculation from the function to the
     original call and print it out.
     
-
     Gotten stuck, look at the slides at:
     https://letsdodigital.org/learn/learn-python/module-1/3-python-basics.html
 """
